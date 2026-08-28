7c06f46f7aa61c2a1545102ee02fa69181449387
Hiryus <hiryus.sha@gmail.com>
2026-08-29 00:43:45 +0200

    feat(stats): aggregate maintainer points per user

---
+++ app/DTO/MaintainerSettings.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DTO;
+
+use App\Models\Map;
+
+final readonly class MaintainerSettings
+{
+    public function __construct(
+        public int $points_created,
+        public int $points_updated,
+        public int $points_deleted,
+        public int $minimum_points,
+    ) {}
+
+    public static function fromMap(Map $map): self
+    {
+        return new self(
+            points_created: $map->maintainer_points_created,
+            points_updated: $map->maintainer_points_updated,
+            points_deleted: $map->maintainer_points_deleted,
+            minimum_points: $map->maintainer_minimum_points,
+        );
+    }
+
+    /**
+     * @param  array{points_created: int, points_updated: int, points_deleted: int, minimum_points: int}  $data
+     */
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            points_created: $data['points_created'],
+            points_updated: $data['points_updated'],
+            points_deleted: $data['points_deleted'],
+            minimum_points: $data['minimum_points'],
+        );
+    }
+
+    /**
+     * @return array{points_created: int, points_updated: int, points_deleted: int, minimum_points: int}
+     */
+    public function toArray(): array
+    {
+        return [
+            'points_created' => $this->points_created,
+            'points_updated' => $this->points_updated,
+            'points_deleted' => $this->points_deleted,
+            'minimum_points' => $this->minimum_points,
+        ];
+    }
+}
